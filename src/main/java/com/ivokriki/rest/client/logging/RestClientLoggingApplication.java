318d5301bf29e1a9023c1eafdbafc2658edca9eb
Ivo Kriki <ivo.kriki@outlook.com>
2024-11-01 09:59:53 +0000

    created rest-client-logging

---
+++ src/main/java/com/ivokriki/rest/client/logging/RestClientLoggingApplication.java
@@ -0,0 +1,14 @@
+package com.ivokriki.rest.client.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientLoggingApplication.class, args);
+		
+	}
+
+}
